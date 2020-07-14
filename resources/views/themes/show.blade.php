247f7c2790a95c41e76027efcad60b00b8c5b8c1
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-14 15:44:07 +0300

    Updated: layout & styles

--- resources/views/themes/show.blade.php
+++ resources/views/themes/show.blade.php
@@ -12,17 +12,19 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-9">
-                <div class="card shadow-sm border-0">
-                <img src="{{$theme->screenshot_url}}" class="card-img-top" alt="{{$theme->name}}">
-                    <div class="card-body">
+            <div class="col-md-8">
+                <div class="card border-0 bg-transparent">
+                <img src="{{$theme->screenshot_url}}" class="card-img-top shadow-sm " alt="{{$theme->name}}">
+                    <div class="card-body py-4 px-0">
+                        <h5>Description</h5>
+                        <hr class="mt-0">
                       <p class="card-text">{{$theme->description}}</p>
                       <p class="card-text text-muted">Tags: {{$theme->tags()}}</p>
                     </div>
                   </div>
                 
             </div>
-            <aside class="sidebar shadow-sm col-md-3 px-4 py-4 bg-white rounded">
+            <aside class="sidebar col-md-4 pl-4">
                 <a type="button" href="{{$theme->download_link}}" class="btn btn-block btn-success">Free download</a>
                 <a type="button" target="blank" href="{{$theme->preview_url}}" class="btn btn-block btn-info mt-3">View demo</a>
 
@@ -31,11 +33,11 @@
                     <a target="blank" href="https://wordpress.org/support/theme/{{$theme->slug}}">Support forum</a>
                 </div>
                 <p class="card-text text-muted mt-3">This item is licensed  100% GPL </p>
-                <ul class="list-unstyled">
-                <li>Author: <a target="blank" href="https://wordpress.org/themes/author/{{$theme->vendor->name}}">{{$theme->vendor->name}}</a></li>
-                    <li>Version: {{$theme->version}}</li>
-                    <li>Last updated: {{$theme->last_updated}}</li>
-                    <li>Downloads: {{$theme->downloaded}}</li>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item px-0  bg-transparent">Author: <a target="blank" href="https://wordpress.org/themes/author/{{$theme->vendor->name}}">{{$theme->vendor->name}}</a></li>
+                    <li class="list-group-item px-0 bg-transparent">Version: {{$theme->version}}</li>
+                    <li class="list-group-item px-0 bg-transparent">Last updated: {{$theme->last_updated}}</li>
+                    <li class="list-group-item px-0 bg-transparent">Downloads: {{$theme->downloaded}}</li>
                   </ul>
                   
                   <div class="mt-3">
