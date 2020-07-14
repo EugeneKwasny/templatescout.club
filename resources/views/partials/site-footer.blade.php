cb9339b0fad381940d89a6fd4d69dfb2cc6e3c77
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-15 01:30:31 +0300

    Updated: footer styles

--- resources/views/partials/site-footer.blade.php
+++ resources/views/partials/site-footer.blade.php
@@ -4,39 +4,39 @@
             <div class="widget col-sm mt-4 mt-sm-0">
                 <h6>Company</h6>
                 <ul class="list-unstyled mb-0">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Terms of Use</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">Contact US</a></li>
+                    <li><a class="text-muted" href="#">About</a></li>
+                    <li><a class="text-muted" href="#">Terms of Use</a></li>
+                    <li><a class="text-muted" href="#">Privacy Policy</a></li>
+                    <li><a class="text-muted" href="#">Contact US</a></li>
                 </ul>
             </div>
             <div class="widget col-sm mt-4 mt-sm-0">
                 <h6>Themes</h6>
                 <ul class="list-unstyled mb-0">
-                    <li><a href="#">Latest</a></li>
-                    <li><a href="#">Best selling</a></li>
-                    <li><a href="#">Fetaured</a></li>
-                    <li><a href="#">Creative</a></li>
-                    <li><a href="#">WooCommerce</a></li>
+                    <li><a class="text-muted" href="#">Latest</a></li>
+                    <li><a class="text-muted" href="#">Best selling</a></li>
+                    <li><a class="text-muted" href="#">Fetaured</a></li>
+                    <li><a class="text-muted" href="#">Creative</a></li>
+                    <li><a class="text-muted" href="#">WooCommerce</a></li>
                 </ul>
             </div>
             <div class="widget col-sm mt-4 mt-sm-0">
                 <h6>Community</h6>
                 <ul class="list-unstyled mb-0">
-                    <li><a href="#">Online Help Center</a></li>
-                    <li><a href="#">Tutorials</a></li>
-                    <li><a href="#">Blog</a></li>
+                    <li><a class="text-muted" href="#">Online Help Center</a></li>
+                    <li><a class="text-muted" href="#">Tutorials</a></li>
+                    <li><a class="text-muted" href="#">Blog</a></li>
                 </ul>
             </div>
             <div class="widget col-sm mt-4 mt-sm-0">
                 <h6>Follow us</h6>
                 <ul class="list-unstyled mb-0">
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Themeforest</a></li>
-                    <li><a href="#">CreativeMarket</a></li>
-                    <li><a href="#">WordPress.org</a></li>
-                    <li><a href="#">Behance</a></li>
+                    <li><a class="text-muted" href="#">Facebook</a></li>
+                    <li><a class="text-muted" href="#">Twitter</a></li>
+                    <li><a class="text-muted" href="#">Themeforest</a></li>
+                    <li><a class="text-muted" href="#">CreativeMarket</a></li>
+                    <li><a class="text-muted" href="#">WordPress.org</a></li>
+                    <li><a class="text-muted" href="#">Behance</a></li>
                 </ul>
             </div>
         </div>
@@ -46,7 +46,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 small text-muted">Copyright © 2011-2020 TemplateScout</div>
-            <div class="col-md-6 text-right small text-muted">Hosted by <a href="https://m.do.co/c/d299d8d2c578" target="blank" rel="nofollow">DigitalOcean</a></div>
+            <div class="col-md-6 text-md-right small text-muted mt-3 mt-md-0">Hosted by <a href="https://m.do.co/c/d299d8d2c578" target="blank" rel="nofollow">DigitalOcean</a></div>
         </div>
     </div>
 </footer>
