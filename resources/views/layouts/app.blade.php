86d3094149fd113ef1ec11409a292f7db37dfb79
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-18 23:28:24 +0300

    Added: preload fro GA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,9 @@
 <html lang="en"> 
 <head>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script rel="preconnect" async src="https://www.googletagmanager.com/gtag/js?id=UA-61458036-3"></script>
+{{-- <script rel="preconnect" async src="https://www.googletagmanager.com/gtag/js?id=UA-61458036-3"></script> --}}
+<link rel="preload" href="https://www.googletagmanager.com/gtag/js?id=UA-61458036-3" as script>
+
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
