f4c61d8191a1aac91be703d882ba9d8585cf2259
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-20 09:28:12 +0300

    Added: recent themes page

--- resources/views/partials/site-header.blade.php
+++ resources/views/partials/site-header.blade.php
@@ -17,9 +17,9 @@
           <a class="nav-link" href="#">Apearance</a>
         </li> --}}
       
-      {{-- <li class="nav-item">
+      <li class="nav-item">
         <a class="nav-link" href="/recent" >Recent</a>
-      </li> --}}
+      </li>
         <li class="nav-item">
           <a class="nav-link" href="/featured" >Featured</a>
         </li>
