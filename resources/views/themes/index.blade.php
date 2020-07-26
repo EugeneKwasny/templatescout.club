9cc29afc0a6050c506d59f2b84576c662c3b629b
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-26 21:11:17 +0300

    Added: tags filter

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('title', '100+ Collection of Free WordPress Themes Handpicked by TemplateScout')
-@section('description', 'Download now Collection of Free WordPress Themes Handpicked by TemplateScout!')
+@section('title', $meta_tags['title'])
+@section('description',  $meta_tags['description'])
 @section('styles')
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 @endsection
@@ -9,8 +9,34 @@
     <section class="main mt-5">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <h1>Free WordPress Themes </h1>
+                <div class="col-md-3"></div>
+                <div class="col-md-9">
+                    <h1>{{$heading_h1}}</h1>
+                    @if($page_intro)
+                        <p class="lead">{{$page_intro}}</p>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 mt-4">
+                    <h5>Filter by tags:</h5>
+                    @if($tags)
+                    <ul class="list-unstyled">
+                        @foreach($tags as $tag)
+                        <li>
+                            <a 
+                            @if($currentTag)
+                                class="{!!($tag->slug == $currentTag->slug) ? 'text-dark font-weight-bold' : 'text-muted'!!}" 
+                            @else
+                            class="text-muted" 
+                            @endif
+                                href="{{$tag->tag_path()}}">{{ucfirst($tag->title)}}</a>
+                        </li>
+                        @endforeach
+                    </ul>
+                    @endif
+                </div>
+                <div class="col-md-9 order-first order-md-last">
                     <div class="card-columns mt-4">
                         @foreach ($themes as $theme)
                             @include('themes.grid-item', ['theme' => $theme])
