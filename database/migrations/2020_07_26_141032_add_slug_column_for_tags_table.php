6057bc6849a69aee9940b621aaaa5352a654c729
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-26 21:16:20 +0300

    Added: slud field for tags tagble

---
+++ database/migrations/2020_07_26_141032_add_slug_column_for_tags_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlugColumnForTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->string('slug')->unique();
+            $table->text('description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
