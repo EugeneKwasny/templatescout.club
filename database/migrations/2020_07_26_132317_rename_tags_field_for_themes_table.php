180ff9483cd1771f4a4559579c45b2e6ae381050
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-26 16:37:50 +0300

    Renamed: tags -> specifications

---
+++ database/migrations/2020_07_26_132317_rename_tags_field_for_themes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameTagsFieldForThemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->renameColumn('tags', 'specifications');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
