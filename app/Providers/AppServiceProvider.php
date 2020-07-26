f73b80d733a85a497cbe1830278e8340d919088d
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-27 00:09:42 +0300

    Added: canonical meta tag

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            if(request()->has('page')){
+                $view->with('canonical', url()->current());
+            }else{
+                $view->with('canonical', null);
+            }
+        });
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-61458036-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-61458036-3');
-</script>
-    <title>@yield('title')</title>
-    <meta name="description" content="@yield('description')">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @yield('styles')
-      
+  @if(App::environment('production'))
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-61458036-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-61458036-3');
+    </script>
+  @endif
+
+  <title>@yield('title')</title>
+  <meta name="description" content="@yield('description')">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @if($canonical)
+    <link rel="canonical" href="{{$canonical}}" />
+  @endif
+  @yield('styles')
+    
 
 </head>
 <body class="bg-light">
