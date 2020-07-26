e1d63b6407f02667822f3fd447a82317b831f591
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2020-07-26 21:08:00 +0300

    Added: tag_path & belongsToMany relationship

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function tag_path(){
+        return route('tag_path', ['slug' => $this->slug]);
+    }
+
+    public function themes(){
+        return $this->belongsToMany('App\Theme');
+    }
 }
